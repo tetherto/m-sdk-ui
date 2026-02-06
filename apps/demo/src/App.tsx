9f9382fd5d3a46f4474d2ea2150697233d024475
habrahamyanbf <hrant.abrahamyan@tether.to>
2026-02-06 15:40:53 +0400

    Components, Provide Tabs Final View

--- apps/demo/src/App.tsx
+++ apps/demo/src/App.tsx
@@ -464,18 +464,34 @@ function App(): JSX.Element {
         {/* Tabs  */}
         <section className="demo-section">
           <h2 className="demo-section__title">Tabs</h2>
-          <Tabs defaultValue="tab1">
-            <TabsList>
-              <TabsTrigger value="tab1">Tab 1</TabsTrigger>
-              <TabsTrigger value="tab2">Tab 2</TabsTrigger>
-              <TabsTrigger disabled value="tab3">
-                Tab 3
-              </TabsTrigger>
-            </TabsList>
-            <TabsContent value="tab1">Tab 1 content</TabsContent>
-            <TabsContent value="tab2">Tab 2 content</TabsContent>
-            <TabsContent value="tab3">Tab 3 content</TabsContent>
-          </Tabs>
+          <div className="demo-section__tabs">
+            <Tabs defaultValue="tab1">
+              <TabsList>
+                <TabsTrigger value="tab1">Tab 1</TabsTrigger>
+                <TabsTrigger value="tab2">Tab 2</TabsTrigger>
+                <TabsTrigger value="tab3">Tab 3</TabsTrigger>
+              </TabsList>
+              <TabsContent value="tab1">Tab 1 content</TabsContent>
+              <TabsContent value="tab2">Tab 2 content</TabsContent>
+              <TabsContent value="tab3">Tab 3 content</TabsContent>
+            </Tabs>
+          </div>
+          <div className="demo-section__tabs">
+            <Tabs defaultValue="tab1">
+              <TabsList>
+                <TabsTrigger value="tab1">Tab 1</TabsTrigger>
+                <TabsTrigger disabled value="tab2">
+                  Tab 2
+                </TabsTrigger>
+                <TabsTrigger disabled value="tab3">
+                  Tab 3
+                </TabsTrigger>
+              </TabsList>
+              <TabsContent value="tab1">Tab 1 content</TabsContent>
+              <TabsContent value="tab2">Tab 2 content</TabsContent>
+              <TabsContent value="tab3">Tab 3 content</TabsContent>
+            </Tabs>
+          </div>
         </section>
       </div>
     </div>
--- packages/core/src/components/tabs/index.tsx
+++ packages/core/src/components/tabs/index.tsx
@@ -3,6 +3,10 @@ import * as React from 'react'
 
 import { cn } from '../../utils'
 
+type TabsProps = React.ComponentPropsWithoutRef<typeof TabsPrimitives.Root>
+type TabsListProps = React.ComponentPropsWithoutRef<typeof TabsPrimitives.List>
+type TabsTriggerProps = React.ComponentPropsWithoutRef<typeof TabsPrimitives.Trigger>
+type TabsContentProps = React.ComponentPropsWithoutRef<typeof TabsPrimitives.Content>
 /**
  * Tabs component for organizing content into panels
  *
@@ -11,43 +15,46 @@ import { cn } from '../../utils'
  * <Tabs defaultValue="tab1">
  *   <TabsList>
  *     <TabsTrigger value="tab1">Tab 1</TabsTrigger>
- *     <TabsTrigger value="tab2">Tab 2</TabsTrigger>
- *     <TabsTrigger value="tab3">Tab 3</TabsTrigger>
+ *     <TabsTrigger value="tab2" disabled>Tab 2</TabsTrigger>
  *   </TabsList>
  *   <TabsContent value="tab1">Tab 1 content</TabsContent>
- *   <TabsContent value="tab2">Tab 2 content</TabsContent>
- *   <TabsContent value="tab3">Tab 3 content</TabsContent>
  * </Tabs>
  * ```
  */
-
-type TabsProps = React.ComponentPropsWithoutRef<typeof TabsPrimitives.Root>
-type TabsListProps = React.ComponentPropsWithoutRef<typeof TabsPrimitives.List>
-type TabsTriggerProps = React.ComponentPropsWithoutRef<typeof TabsPrimitives.Trigger>
-type TabsContentProps = React.ComponentPropsWithoutRef<typeof TabsPrimitives.Content>
-
 const Tabs = React.forwardRef<HTMLDivElement, TabsProps>(({ className, ...props }, ref) => (
-  <TabsPrimitives.Root ref={ref} className={cn('mdk-tabs', className)} {...props} />
+  <TabsPrimitives.Root ref={ref} className={cn('mining_sdk_tabs', className)} {...props} />
 ))
+
 Tabs.displayName = 'Tabs'
 
 const TabsList = React.forwardRef<HTMLDivElement, TabsListProps>(({ className, ...props }, ref) => (
-  <TabsPrimitives.List ref={ref} className={cn('mdk-tabs__list', className)} {...props} />
+  <TabsPrimitives.List ref={ref} className={cn('mining_sdk_tabs__list', className)} {...props} />
 ))
+
 TabsList.displayName = 'TabsList'
 
 const TabsTrigger = React.forwardRef<HTMLButtonElement, TabsTriggerProps>(
   ({ className, ...props }, ref) => (
-    <TabsPrimitives.Trigger ref={ref} className={cn('mdk-tabs__trigger', className)} {...props} />
+    <TabsPrimitives.Trigger
+      ref={ref}
+      className={cn('mining_sdk_tabs__trigger', className)}
+      {...props}
+    />
   ),
 )
+
 TabsTrigger.displayName = 'TabsTrigger'
 
 const TabsContent = React.forwardRef<HTMLDivElement, TabsContentProps>(
   ({ className, ...props }, ref) => (
-    <TabsPrimitives.Content ref={ref} className={cn('mdk-tabs__content', className)} {...props} />
+    <TabsPrimitives.Content
+      ref={ref}
+      className={cn('mining_sdk_tabs__content', className)}
+      {...props}
+    />
   ),
 )
+
 TabsContent.displayName = 'TabsContent'
 
 export { Tabs, TabsContent, TabsList, TabsTrigger }
